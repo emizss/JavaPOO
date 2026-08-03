f3f47da43e695bd923660e98d37ff3a6b2d70fdc
emilli <emillisant00oo@gmail.com>
2026-08-03 16:22:10 -0300

    criei a classe, construtor, getters e setters

--- Atividades-aula/src/avaliacao2/atv2/Estudante.java
+++ Atividades-aula/src/avaliacao2/atv2/Estudante.java
@@ -1,4 +1,30 @@
 package avaliacao2.atv2;
+import java.util.ArrayList;
 
 public class Estudante {
+    private String nome;
+    private  ArrayList<Double> notas;
+
+
+    public Estudante(String nome){
+        this.nome = nome;
+        this.notas = new ArrayList();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Double> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(ArrayList<Double> notas) {
+        this.notas = notas;
+    }
 }
+
