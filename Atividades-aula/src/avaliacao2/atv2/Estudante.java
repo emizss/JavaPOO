5b83ce922a29c10f663225203dd5c991e1df6876
Emilli <emillisant00oo@gmail.com>
2026-08-24 12:22:21 -0300

    realizei a atividade 4, fazendo uma sobrecarga ao metodo calculaMedia

--- Atividades-aula/src/avaliacao2/atv2/Estudante.java
+++ Atividades-aula/src/avaliacao2/atv2/Estudante.java
@@ -6,11 +6,13 @@ public class Estudante {
     private String nome;
     private  ArrayList<Double> notas;
     private Double media;
+    private  ArrayList<Integer> pesos;
 
     public Estudante(String nome){
-        this.media = media;
+        this.media = 0.0;
         this.nome = nome;
-        this.notas = new ArrayList<Double>();
+        this.notas = new ArrayList<>();
+        this.pesos = new ArrayList<>();
     }
 
     public String getNome() {
@@ -29,31 +31,47 @@ public class Estudante {
         this.notas = notas;
     }
 
+    public ArrayList<Integer> getPesos() {
+        return pesos;
+    }
+
+    public void setPesos(ArrayList<Integer> pesos) {
+        this.pesos = pesos;
+    }
+
     public Double getMedia() {
         return media;
     }
-    public void setMedia(){this.media = media;}
+    public void setMedia(double media){this.media = media;}
 
     public int tamanhoArray(){
         int tam = notas.size();
         return tam;
     }
+
     public void insereNotas(double nota){
         int tamanho = tamanhoArray();
-        if (tamanho <= 4){
+        if (tamanho < 5){
             this.notas.add(nota);
         }else{
-            System.out.println("As 5 notas já foram preenchidas. Caso queira adicionar uma, é necessário excluir.");
+            System.out.println("As 5 notas já foram preenchidas.");
+        }
+    }
+    public void inserePesos(int peso){
+        if (pesos.size() < 5){
+            this.pesos.add(peso);
+        }else{
+            System.out.println("Os 5 pesos já foram preenchidos.");
         }
     }
     public double calculaSoma(ArrayList<Double> notas){
-        double soma;
-        soma = 0;
+        double soma = 0;
         for (double valor : notas){
             soma += valor;
         }
         return soma;
     }
+    //média aritmética
     public double calculaMedia(ArrayList<Double> notas){
         int tamanho = tamanhoArray();
         media= calculaSoma(notas)/tamanho;
@@ -65,10 +83,29 @@ public class Estudante {
         return menorNota;
     }
 
+    @Override
     public String toString(){
-
-        return String.format("Nome: %s e média  %.2f", nome, media);
+        return String.format("Nome: %s e média:  %.2f", nome, media);
     }
 
+    public int calculaPesoTotal(ArrayList<Integer> pesos){
+        int pesoT = 0;
+        for(int peso : pesos){
+            pesoT+=peso;
+        }
+        return pesoT;
+    }
+
+    //media ponderada
+    public double calculaMedia(ArrayList<Double> notas, ArrayList<Integer> pesosArray){
+        double somaNotasEPesos = 0.0;
+        int pesoTotal = calculaPesoTotal(pesosArray);
+
+        for (int i = 0; i < pesosArray.size(); i++) {
+            somaNotasEPesos += notas.get(i) * pesosArray.get(i);
+        }
+        media = somaNotasEPesos / pesoTotal;
+        return media;
+    }
 }
 
