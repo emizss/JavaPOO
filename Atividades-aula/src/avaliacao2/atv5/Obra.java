e67388bc1aec8b4943647045aa157fb7938fcdfc
Emilli <emillisant00oo@gmail.com>
2026-08-25 14:40:18 -0300

    criei a classe obra

---
+++ Atividades-aula/src/avaliacao2/atv5/Obra.java
@@ -0,0 +1,18 @@
+package avaliacao2.atv5;
+
+public class Obra {
+
+    private String titulo;
+
+    public Obra(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
